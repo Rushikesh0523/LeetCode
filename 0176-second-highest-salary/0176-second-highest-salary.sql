026a7138f4068778bab686985fe7e39bb2aaa670
Rushikesh Pradip Patil <163570727+Rushikesh0523@users.noreply.github.com>
2025-06-18 12:46:21 +0530

    Time: 915 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,17 +1,7 @@
 /* Write your T-SQL query statement below */
 
-/*Select max(salary) as SecondHighestSalary
+Select max(salary) as SecondHighestSalary
 from employee
 where salary not in (
     select max(salary) from employee
-);*/
-
-with cte as(
-    select salary, Dense_rank()
-    over (order by salary desc) as rnk
-    from employee
-    
-)
-select max(salary) as SecondHighestSalary
-from cte 
-where rnk = 2;
+);
